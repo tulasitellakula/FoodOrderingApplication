9faa8857790f3a0e944dce963551040b3a67adaf
tulasi <lakshmitulasitellakula@gmail.com>
2023-09-12 10:04:27 +0530

    added Frontend and backend

---
+++ frontend/src/component/AllProduct.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import CardFeature from "./CardFeature";
+import FilterProduct from "./FilterProduct";
+
+const AllProduct = ({ heading }) => {
+  const productData = useSelector((state) => state.product.productList);
+  const categoryList = [...new Set(productData.map((el) => el.category))];
+
+  //filter data display
+  const [filters,setFilters] = useState({
+      filterby:"",
+      dataFilter:[]
+  });
+
+  useEffect(() => {
+    setFilters({dataFilter:productData,filterby:""});
+  }, [productData]);
+
+  const handleFilterProduct = (category) => {
+    if(category === filters.filterby){category="";}
+    console.log(category);
+    let filter = productData.filter(
+      (el) => el.category.toLowerCase() === category.toLowerCase()
+    );
+    if(category===""){filter=productData;}
+      setFilters({
+        dataFilter:filter,filterby:category
+      })
+  };
+
+  const loadingArrayFeature = new Array(10).fill(null);
+
+  return (
+    <div className="my-5">
+      <h2 className="font-bold text-2xl text-slate-800 mb-4 ml-10">{heading}</h2>
+
+      <div className="flex gap-4 justify-center overflow-scroll scrollbar-none">
+        {categoryList[0] ? (
+          categoryList.map((el) => {
+            return (
+              <FilterProduct
+                category={el}
+                key={el}
+                isActive={el.toLowerCase() === filters.filterby.toLowerCase()}
+                onClick={() => handleFilterProduct(el)}
+              />
+            );
+          })
+        ) : (
+          <div className="min-h-[150px] flex justify-center items-center">
+            <p>Loading...</p>
+          </div>
+        )}
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-4 my-4">
+        {filters.dataFilter[0]
+          ? filters.dataFilter.map((el) => {
+              return (
+                <CardFeature
+                  key={el._id}
+                  id={el._id}
+                  image={el.image}
+                  name={el.name}
+                  category={el.category}
+                  price={el.price}
+                />
+              );
+            })
+          : 
+          loadingArrayFeature.map((el,index) => (
+              <CardFeature loading="Loading..." key={index+"allProduct"} />
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default AllProduct;
